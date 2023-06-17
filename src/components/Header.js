558d94654f7e88989f447a7cc6ddea2b19ce6fa4
William Gay <williamgay360@gmail.com>
2023-06-17 15:05:01 -0400

    chore: fucked up links

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ function Header() {
         <h1>toolbox</h1>
         <nav>
         <ul>
-            <li><a href="https://tools.williamgay.me">home</a></li>
-            <li><a href="https://tools.williamgay.me/blog">blog</a></li>
-            <li><a href="https://tools.williamgay.me/books">books</a></li>
+            <li><a href="https://williamgay.me">home</a></li>
+            <li><a href="https://williamgay.me/blog">blog</a></li>
+            <li><a href="https://williamgay.me/books">books</a></li>
             <li><a href="https://tools.williamgay.me">tools</a></li>
             <li><a href="https://github.com/williamgay25">github</a></li>
             <li><a href="https://twitter.com/williamgay25">twitter</a></li>
