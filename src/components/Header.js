6cc53f8eb1b37698c317a8ce9ca47c7a8c4163bf
William Gay <williamgay360@gmail.com>
2023-06-17 14:41:05 -0400

    feat: fixed up the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,14 @@ import './Header.css';
 function Header() {
   return (
     <header>
-      <h1>toolbox</h1>
-      <p>by william gay</p>
-      <nav>
-        <ul>
-          <li><a href="https://williamgay.me">home</a></li>
-          <li><a href="https://github.com/williamgay25">github</a></li>
-          <li><a href="https://twitter.com/williamgay25">twitter</a></li>
-          <li><a href="https://www.linkedin.com/in/williamegay/">linkedIn</a></li>
-        </ul>
-      </nav>
+      <div className="header-container">
+        <h1>toolbox</h1>
+        <h5>by <a href="https://williamgay.me">william gay</a></h5>
+      </div>
+      <p><a href="https://github.com/williamgay25/toolbox" className="source-code">source code</a></p>
     </header>
   );
 }
 
 export default Header;
+
