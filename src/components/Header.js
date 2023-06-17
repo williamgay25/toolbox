3548255b10e87181226d3658d4e0a1b4501153fb
William Gay <williamgay360@gmail.com>
2023-06-17 15:10:00 -0400

    chore: links still busted

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ function Header() {
         <nav>
         <ul>
             <li><a href="https://williamgay.me">home</a></li>
-            <li><a href="https://williamgay.me/blog">blog</a></li>
-            <li><a href="https://williamgay.me/books">books</a></li>
+            <li><a href="https://williamgay.me/blog.html">blog</a></li>
+            <li><a href="https://williamgay.me/books.html">books</a></li>
             <li><a href="https://tools.williamgay.me">tools</a></li>
             <li><a href="https://github.com/williamgay25">github</a></li>
             <li><a href="https://twitter.com/williamgay25">twitter</a></li>
